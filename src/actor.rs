49b681e01edb91cf2719a048a790941c7a6253dd
Belousow Makc <lib.bmw@gmail.com>
2020-07-30 17:24:24 +0300

    Add actor docs.

--- src/actor.rs
+++ src/actor.rs
@@ -8,6 +8,28 @@ use log::{debug, info};
 
 use crate::CacheError;
 
+/// Actix actor implements cache logic.
+///
+/// This actor implement only `Handler<QueryCache>`.
+/// Where [QueryCache] - Actix message with two fields:
+/// * Generic actix message for sending to upstream actor.
+/// * Address of upstream actor
+///
+/// # Example
+/// ```rust
+/// use actix::prelude::*;
+/// use actix_cache::{Cache as CacheActor, RedisBackend, CacheError};
+///
+/// type Cache = CacheActor<RedisBackend>;
+///
+/// #[actix_rt::main]
+/// async fn main() -> Result<(), CacheError> {
+///     let cache = Cache::new().await?.start();
+///     Ok(())
+/// }
+/// ```
+///
+/// [QueryCache]: ../cache/struct.QueryCache.html
 pub struct Cache<B>
 where
     B: Backend,
@@ -20,6 +42,9 @@ impl<B> Cache<B>
 where
     B: Backend,
 {
+    /// Initialize new Cache actor with default [RedisBackend].
+    ///
+    /// [RedisBackend]: ../../actix_cache_redis/actor/struct.RedisActor.html
     pub async fn new() -> Result<Cache<RedisActor>, CacheError> {
         let backend = RedisActor::new()
             .await
@@ -28,6 +53,7 @@ where
         Ok(CacheBuilder::default().build(backend))
     }
 
+    /// Creates new [CacheBuilder](struct.CacheBuilder.html) instance for Cache actor configuration.
     pub fn builder() -> CacheBuilder<B> {
         CacheBuilder::default()
     }
@@ -45,6 +71,25 @@ where
     }
 }
 
+/// Cache actor configurator.
+///
+/// # Example
+/// ```rust
+/// use actix::prelude::*;
+/// use actix_cache::{Cache, RedisBackend, CacheError};
+///
+/// #[actix_rt::main]
+/// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+///     let backend = RedisBackend::new()
+///         .await?
+///         .start();
+///     let cache = Cache::builder()
+///         .enabled(true)
+///         .build(backend)
+///         .start();
+///     Ok(())
+/// }
+/// ```
 pub struct CacheBuilder<B>
 where
     B: Backend + Actor,
@@ -69,11 +114,25 @@ impl<B> CacheBuilder<B>
 where
     B: Backend,
 {
+    /// Enable or disable interaction with cache backend.
+    ///
+    /// All messages sent to disabled Cache actor passed directly to an upstream actor.
+    /// If metrics feature is enabled, metrics for these messages collected too.
     pub fn enabled(mut self, enabled: bool) -> Self {
         self.enabled = enabled;
         self
     }
 
+    /// Instatiate new [Cache] instance with current configuration and passed backend.
+    ///
+    /// Backend is an [Addr] of actix [Actor] which implements [Backend] trait:
+    ///
+    /// [Cache]: actor/struct.Cache.html
+    /// [Backend]: ../dev/trait.Backend.html
+    /// [Addr]: https://docs.rs/actix/latest/actix/prelude/struct.Addr.html
+    /// [Actor]: https://docs.rs/actix/latest/actix/prelude/trait.Actor.html
+    /// [Messages]: https://docs.rs/actix/latest/actix/prelude/trait.Message.html
+    /// [Handler]: https://docs.rs/actix/latest/actix/prelude/trait.Handler.html
     pub fn build(self, backend: Addr<B>) -> Cache<B> {
         Cache {
             enabled: self.enabled,
