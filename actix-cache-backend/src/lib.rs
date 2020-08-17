deb462bb7716807c3e44c327a47784ce43da925b
Belousow Makc <lib.bmw@gmail.com>
2020-08-17 16:38:08 +0300

    Add actix-cache-backend docs.

--- actix-cache-backend/src/lib.rs
+++ actix-cache-backend/src/lib.rs
@@ -1,16 +1,36 @@
+#![warn(missing_docs)]
+//! Traits and struct messages for actix-cache backend interaction.
+//!
+//! If you want implement your own backend, you in the right place.
 use actix::dev::ToEnvelope;
 use actix::prelude::*;
 use thiserror::Error;
 
+/// Define the behavior needed of an cache layer to work with cache backend.
+///
+/// Ultimately the implementing type must be an Actix `Actor` and it must implement handlers for a
+/// specific set of message types:
+///
+/// * [Get]
+/// * [Set]
+/// * [Lock]
+/// * [Delete]
+///
+/// [Get]: ./struct.Get.html
+/// [Set]: ./struct.Set.html
+/// [Delete]: ./struct.Delete.html
+/// [Lock]: ./struct.Lock.html
 pub trait Backend
 where
     Self: Actor + Handler<Set> + Handler<Get> + Handler<Lock> + Handler<Delete>,
 {
+    /// Type of backend actor bound.
     type Actor: Actor<Context = <Self as Backend>::Context>
         + Handler<Set>
         + Handler<Get>
         + Handler<Lock>
         + Handler<Delete>;
+    /// Type for backend Actor context.
     type Context: ActorContext
         + ToEnvelope<Self::Actor, Get>
         + ToEnvelope<Self::Actor, Set>
@@ -18,10 +38,15 @@ where
         + ToEnvelope<Self::Actor, Delete>;
 }
 
+/// Proxy Error describes general groups of errors in backend interaction process.
 #[derive(Debug, Error)]
 pub enum BackendError {
+    /// Internal backend error, state or computation error.
+    ///
+    /// Any error not bounded with network interaction.
     #[error(transparent)]
     InternalError(Box<dyn std::error::Error + Send>),
+    /// Network interaction error.
     #[error(transparent)]
     ConnectionError(Box<dyn std::error::Error + Send>),
 }
@@ -30,6 +55,7 @@ pub enum BackendError {
 #[derive(Message, Debug, Clone, PartialEq)]
 #[rtype(result = "Result<Option<String>, BackendError>")]
 pub struct Get {
+    /// Key of cache backend record.
     pub key: String,
 }
 
@@ -37,8 +63,11 @@ pub struct Get {
 #[derive(Message, Debug, Clone, PartialEq)]
 #[rtype(result = "Result<String, BackendError>")]
 pub struct Set {
+    /// Key of cache backend record.
     pub key: String,
+    /// Data for sorage by cache key.
     pub value: String,
+    /// Optional value of time-to-live for cache record.
     pub ttl: Option<u32>,
 }
 
@@ -55,6 +84,7 @@ pub enum DeleteStatus {
 #[derive(Message, Debug, Clone, PartialEq)]
 #[rtype(result = "Result<DeleteStatus, BackendError>")]
 pub struct Delete {
+    /// Key of cache backend record for deleting
     pub key: String,
 }
 
@@ -62,7 +92,9 @@ pub struct Delete {
 #[derive(Message, Debug, Clone, PartialEq)]
 #[rtype(result = "Result<LockStatus, BackendError>")]
 pub struct Lock {
+    /// Key of cache backend record for lock.
     pub key: String,
+    /// Time-to-live for cache key lock record.
     pub ttl: u32,
 }
 
