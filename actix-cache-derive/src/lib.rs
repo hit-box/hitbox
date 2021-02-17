4519f8fff94ab59e0d1a9891966319bd98d4fada
Andrey Ermilov <andrerm@ya.ru>
2021-02-17 23:44:04 +0300

    master: Fix bug: could not find export in syn

--- actix-cache-derive/src/lib.rs
+++ actix-cache-derive/src/lib.rs
@@ -18,7 +18,7 @@
 //! ```
 mod cacheable_macro;
 mod macro_attributes;
-use syn::export::TokenStream;
+use proc_macro::TokenStream;
 
 /// Derive Cacheable macro implementation.
 #[proc_macro_derive(Cacheable, attributes(cache_ttl, cache_stale_ttl, cache_version))]
