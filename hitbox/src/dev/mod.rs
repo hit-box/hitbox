cf79842b3481b639011190d256764c4b94a0f87f
Andrey Ermilov <amermilov@avito.ru>
2021-05-10 16:28:53 +0300

    state-machine: Fix some tests

--- hitbox/src/dev/mod.rs
+++ hitbox/src/dev/mod.rs
@@ -1,5 +1,5 @@
 mod mock_adapter;
 mod mock_backend;
 
-// pub use adapter::MockAdapter;
+pub use mock_adapter::MockAdapter;
 pub use mock_backend::{Backend, BackendError, Delete, Get, Lock, LockStatus, Set};
