58b6044c7332c4d16af3db5c9c343521490c296e
Andrey Ermilov <andrerm@ya.ru>
2021-05-21 20:53:51 +0300

    Fix path to uml

--- hitbox/src/transition_groups/mod.rs
+++ hitbox/src/transition_groups/mod.rs
@@ -1,7 +1,7 @@
 //! Module that implements transitions between states of the Hitbox finite state machine.
-/// transition [Transition diagram](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/hit-box/hitbox/issue-14/hitbox/diagrams/only_cache.puml)
+/// transition [Transition diagram](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/hit-box/hitbox/master/hitbox/diagrams/only_cache.puml)
 pub mod only_cache;
-/// transition [Transition diagram](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/hit-box/hitbox/issue-14/hitbox/diagrams/stale.puml)
+/// transition [Transition diagram](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/hit-box/hitbox/master/hitbox/diagrams/stale.puml)
 pub mod stale;
-/// transition [Transition diagram](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/hit-box/hitbox/issue-14/hitbox/diagrams/upstream.puml)
+/// transition [Transition diagram](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/hit-box/hitbox/master/hitbox/diagrams/upstream.puml)
 pub mod upstream;
