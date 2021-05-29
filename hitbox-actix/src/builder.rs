76250fe9b6291b94dcdefbb20ab41860e17e551a
Andrey Ermilov <andrerm@ya.ru>
2021-05-29 18:10:54 +0300

    Add docs for hitbox-actix

--- hitbox-actix/src/builder.rs
+++ hitbox-actix/src/builder.rs
@@ -39,7 +39,7 @@ where
         CacheBuilder {
             settings: CacheSettings {
                 cache: Status::Enabled,
-                stale: Status::Disabled,
+                stale: Status::Enabled,
                 lock: Status::Disabled,
             },
             _p: PhantomData::default(),
@@ -51,31 +51,50 @@ impl<B> CacheBuilder<B>
 where
     B: Backend,
 {
+    /// Enable interaction with cache backend. (Default value).
     pub fn enable(mut self) -> Self {
         self.settings.cache = Status::Enabled;
         self
     }
 
+    /// Disable interaction with cache backend.
+    ///
+    /// All messages sent to disabled Cache actor passed directly to an upstream actor.
     pub fn disable(mut self) -> Self {
         self.settings.cache = Status::Disabled;
         self
     }
 
+    /// Enable stale cache mechanics. (Default value).
+    ///
+    /// If [CacheActor] receives a stale value, it does not return it immediately.
+    /// It polls data from upstream, and if the upstream returned an error,
+    /// the [CacheActor] returns a stale value. If no error occurred in the upstream,
+    /// then a fresh value is stored in the cache and returned.
     pub fn with_stale(mut self) -> Self {
         self.settings.stale = Status::Enabled;
         self
     }
 
+    /// Disable stale cache mechanics.
     pub fn without_stale(mut self) -> Self {
         self.settings.stale = Status::Disabled;
         self
     }
 
+    /// Enable cache lock mechanics.
+    ///
+    /// Prevents multiple upstream requests for the same cache key in case of cache data is missing.
+    /// Only the first request will produce an upstream request.
+    /// The remaining requests wait for a first upstream response and return updated data.
+    /// If `with_stale` is enabled the remaining requests don't wait for an upstream response
+    /// and return stale cache data if it exists.
     pub fn with_lock(mut self) -> Self {
         self.settings.lock = Status::Enabled;
         self
     }
 
+    /// Disable cache lock mechanics. (Default value).
     pub fn without_lock(mut self) -> Self {
         self.settings.lock = Status::Disabled;
         self
@@ -85,8 +104,8 @@ where
     ///
     /// Backend is an [Addr] of actix [Actor] which implements [Backend] trait:
     ///
-    /// [Cache]: actor/struct.Cache.html
-    /// [Backend]: ../dev/trait.Backend.html
+    /// [Cache]: crate::Cache
+    /// [Backend]: hitbox_backend::Backend
     /// [Addr]: https://docs.rs/actix/latest/actix/prelude/struct.Addr.html
     /// [Actor]: https://docs.rs/actix/latest/actix/prelude/trait.Actor.html
     /// [Messages]: https://docs.rs/actix/latest/actix/prelude/trait.Message.html
