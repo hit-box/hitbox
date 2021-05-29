50c5592c975e3439717c2e08181afe13c3fcde3e
Belousov Maksim <lib.bmw@gmail.com>
2021-05-29 18:58:33 +0300

    Update some docs.

--- hitbox/src/states/mod.rs
+++ hitbox/src/states/mod.rs
@@ -1,14 +1,14 @@
 //! The set of states of the Hitbox finite state machine.
 //!
 //! # Motivation
-//! Different caching options are suitable for different [load profiles].
+//! Different caching options are suitable for different load profiles.
 //! We devised Hitbox as a one-stop solution and therefore provide several different caching policies.
-//! They are all described in [InitialCacheSettings].
+//! They are all described in [CacheSettings].
 //! The behavior of Hitbox depends on which caching policy option is selected, which means that
-//! all Hitbox actions depend on the initial state ([InitialCacheSettings]).
+//! all Hitbox actions depend on the [Initial] state.
 //! To implement caching logic that depends on the initial state, we chose a finite state machine.
 //! State machine transitions are defined by the [transition_groups] module and correspond
-//! to the [InitialCacheSettings] options.
+//! to the [CacheSettings] options.
 //!
 //! States shouldn't depend on specific implementations of the cache backend and upstream,
 //! and to interact with them, many states contain an `adapter` field.
@@ -18,9 +18,9 @@
 //! # States
 //! The states module is a set of states of the Hitbox finite state machine.
 //!
-//! [transition_groups]: ../transition_groups/index.html
-//! [InitialCacheSettings]: ../settings/enum.InitialCacheSettings.html
-//! [load profiles]: ../
+//! [transition_groups]: crate::transition_groups
+//! [CacheSettings]: crate::settings::CacheSettings
+//! [Initial]: crate::states::initial::Initial
 
 /// Defines whether the result returned from upstream will be cached.
 pub mod cache_policy;
