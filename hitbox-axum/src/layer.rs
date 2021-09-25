ebfdfff5aaedd17b673d17adb9f5b0c9fc787df4
lowit <ea@lowit.ru>
2021-09-25 04:52:12 +0300

    issue-25-axum: adds CacheLayerBuilder

--- hitbox-axum/src/layer.rs
+++ hitbox-axum/src/layer.rs
@@ -1,9 +1,6 @@
 use crate::service::CacheService;
-use tower_layer::Layer;
 use crate::CacheableRequest;
-
-#[derive(Debug, Default)]
-pub struct CacheConfig;
+use tower_layer::Layer;
 
 #[derive(Debug, Default)]
 pub struct CacheLayer {
@@ -11,12 +8,11 @@ pub struct CacheLayer {
     ttl: Option<u32>,
     stale_ttl: Option<u32>,
     cache_version: Option<u32>,
-    cache_config: CacheConfig,
 }
 
 impl CacheLayer {
-    pub fn new() -> Self {
-        CacheLayer::default()
+    pub fn build() -> CacheLayerBuilder {
+        CacheLayerBuilder::default()
     }
 }
 
@@ -25,5 +21,41 @@ impl<S> Layer<S> for CacheLayer {
 
     fn layer(&self, service: S) -> Self::Service {
         CacheService::new(service)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct CacheLayerBuilder {
+    key_prefix: Option<String>,
+    ttl: Option<u32>,
+    stale_ttl: Option<u32>,
+    version: Option<u32>,
+}
+
+impl CacheLayerBuilder {
+    pub fn with_key_prefix(mut self, prefix: &str) -> CacheLayerBuilder {
+        self.key_prefix = Some(prefix.to_string());
+        self
+    }
+    pub fn with_ttl(mut self, ttl: u32) -> CacheLayerBuilder {
+        self.ttl = Some(ttl);
+        self
+    }
+    pub fn with_stale(mut self, stale_ttl: u32) -> CacheLayerBuilder {
+        self.stale_ttl = Some(stale_ttl);
+        self
+    }
+    pub fn with_version(mut self, version: u32) -> CacheLayerBuilder {
+        self.version = Some(version);
+        self
+    }
+
+    pub fn finish(self) -> CacheLayer {
+        CacheLayer {
+            cache_key_prefix: self.key_prefix,
+            ttl: self.ttl,
+            stale_ttl: self.stale_ttl,
+            cache_version: self.version,
+        }
     }
 }
