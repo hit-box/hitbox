92e5167910a0fe15a76c7f3921e33722c00db582
Denis Kayshev <topenkoff@gmail.com>
2022-04-13 03:03:39 +0300

    feat/metrics: add tests for miss and stale counter

--- hitbox/tests/metrics.rs
+++ hitbox/tests/metrics.rs
@@ -1,7 +1,7 @@
 #[cfg(all(test, feature = "cache-metrics"))]
 mod tests {
     use hitbox::dev::MockAdapter;
-    use hitbox::metrics::CACHE_HIT_COUNTER;
+    use hitbox::metrics::{CACHE_HIT_COUNTER, CACHE_MISS_COUNTER, CACHE_STALE_COUNTER};
     use hitbox::settings::Status;
     use hitbox::states::initial::Initial;
     use metrics::{Counter, Gauge, Histogram, Key, KeyName, Label, Recorder, Unit};
@@ -57,6 +57,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_hit_counter() {
+        metrics::clear_recorder();
         let recorder = MockRecorder::new();
         let handler = recorder.clone();
         metrics::set_boxed_recorder(Box::new(recorder)).unwrap();
@@ -66,16 +67,65 @@ mod tests {
             lock: Status::Disabled,
         };
         let adapter = MockAdapter::build()
-            .with_upstream_value(42)
-            .with_cache_actual(41)
+            .with_cache_actual(42)
             .finish();
         let initial_state = Initial::new(settings.clone(), adapter.clone());
         let _ = initial_state.transitions().await.unwrap();
 
-        let metrics_key = Key::from_parts(CACHE_HIT_COUNTER.as_ref(), LABELS.to_vec());
+        let hit_key = Key::from_parts(CACHE_HIT_COUNTER.as_ref(), LABELS.to_vec());
         let counters = handler.registry.get_counter_handles();
-        let counter = counters.get(&metrics_key);
-        assert!(counter.is_some());
-        assert_eq!(counter.unwrap().load(Ordering::Acquire), 1);
+        let hit_counter = counters.get(&hit_key);
+        assert!(hit_counter.is_some());
+        assert_eq!(hit_counter.unwrap().load(Ordering::Acquire), 1);
+    }
+
+    #[tokio::test]
+    async fn test_miss_counter() {
+        metrics::clear_recorder();
+        let recorder = MockRecorder::new();
+        let handler = recorder.clone();
+        metrics::set_boxed_recorder(Box::new(recorder)).unwrap();
+        let settings = hitbox::settings::CacheSettings {
+            cache: Status::Enabled,
+            stale: Status::Disabled,
+            lock: Status::Disabled,
+        };
+        let adapter = MockAdapter::build()
+            .with_cache_miss()
+            .with_upstream_value(42)
+            .finish();
+        let initial_state = Initial::new(settings.clone(), adapter.clone());
+        let _ = initial_state.transitions().await.unwrap();
+
+        let miss_key = Key::from_parts(CACHE_MISS_COUNTER.as_ref(), LABELS.to_vec());
+        let counters = handler.registry.get_counter_handles();
+        let miss_counter = counters.get(&miss_key);
+        assert!(miss_counter.is_some());
+        assert_eq!(miss_counter.unwrap().load(Ordering::Acquire), 1);
+    }
+
+    #[tokio::test]
+    async fn test_stale_counter() {
+        metrics::clear_recorder();
+        let recorder = MockRecorder::new();
+        let handler = recorder.clone();
+        metrics::set_boxed_recorder(Box::new(recorder)).unwrap();
+        let settings = hitbox::settings::CacheSettings {
+            cache: Status::Enabled,
+            stale: Status::Enabled,
+            lock: Status::Disabled,
+        };
+        let adapter = MockAdapter::build()
+            .with_cache_stale(41, chrono::Utc::now())
+            .with_upstream_value(42)
+            .finish();
+        let initial_state = Initial::new(settings.clone(), adapter.clone());
+        let _ = initial_state.transitions().await.unwrap();
+
+        let stale_key = Key::from_parts(CACHE_STALE_COUNTER.as_ref(), LABELS.to_vec());
+        let counters = handler.registry.get_counter_handles();
+        let stale_counter = counters.get(&stale_key);
+        assert!(stale_counter.is_some());
+        assert_eq!(stale_counter.unwrap().load(Ordering::Acquire), 1);
     }
 }
