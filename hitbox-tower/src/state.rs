45f47cde7cf268eb4103eb1c2d2778d686a84410
Belousov Maksim <lib.bmw@gmail.com>
2023-01-29 16:19:45 +0300

    feat(tower): MVP new state machine

--- hitbox-tower/src/state.rs
+++ hitbox-tower/src/state.rs
@@ -32,21 +32,44 @@ where
 }
 
 pin_project! {
-    pub struct FutureResponse<PollUpstream> {
-        #[pin]
-        state: CacheState<PollUpstream>,
+    #[project = StateProj]
+    pub enum State<Res, PollUpstream> {
+        Inital {
+            #[pin]
+            poll_upstream: PollUpstream
+        },
+        UpstreamPolled {
+            upstream_result: Option<Res>,
+        },
     }
 }
 
 pin_project! {
-    #[project = CacheStateProj]
-    enum CacheState<PollUpstream> {
-        // CachePoll {
-            // poll_cache: PollCache
-        // },
-        UpstreamPoll {
-            #[pin]
-            poll_upstream: PollUpstream
+    pub struct FutureResponse<PollUpstream>
+    where
+        PollUpstream: Future,
+    {
+        #[pin]
+        state: State<PollUpstream::Output, PollUpstream>,
+    }
+}
+
+impl<Res, PollUpstream> Debug for State<Res, PollUpstream> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            State::Inital { poll_upstream: _ } => f.write_str("State::Initial"),
+            State::UpstreamPolled { upstream_result: _ } => f.write_str("State::UpstreamPolled"),
+        }
+    }
+}
+
+impl<PollUpstream> FutureResponse<PollUpstream>
+where
+    PollUpstream: Future,
+{
+    fn new(poll_upstream: PollUpstream) -> Self {
+        FutureResponse {
+            state: State::Inital { poll_upstream },
         }
     }
 }
@@ -54,17 +77,25 @@ pin_project! {
 impl<PollUpstream> Future for FutureResponse<PollUpstream>
 where
     PollUpstream: Future,
+    PollUpstream::Output: Debug,
 {
     type Output = PollUpstream::Output;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let mut this = self.project();
-        
-        // let adapter = FutureAdapter::new(poll_upstream, req, self.backend);
 
-        match this.state.as_mut().project() {
-            CacheStateProj::UpstreamPoll { poll_upstream } => {
-                return Poll::Ready(ready!(poll_upstream.poll(cx)))
+        loop {
+            dbg!(&this.state.as_ref());
+            match this.state.as_mut().project() {
+                StateProj::Inital { poll_upstream } => {
+                    let upstream_result = ready!(poll_upstream.poll(cx));
+                    this.state.set(State::UpstreamPolled {
+                        upstream_result: Some(upstream_result),
+                    });
+                }
+                StateProj::UpstreamPolled { upstream_result } => {
+                    return Poll::Ready(upstream_result.take().unwrap());
+                }
             }
         }
     }
@@ -74,6 +105,7 @@ impl<Req, S, B, PollUpstream> Service<Request<Req>> for CacheService<S, B>
 where
     S: Service<Request<Req>, Future = PollUpstream>,
     PollUpstream: Future<Output = Result<S::Response, S::Error>>,
+    PollUpstream::Output: Debug,
 
     Request<Req>: Debug,
 {
@@ -90,10 +122,7 @@ where
 
     fn call(&mut self, req: Request<Req>) -> Self::Future {
         dbg!(&req);
-        let state = CacheState::UpstreamPoll {
-            poll_upstream: self.upstream.call(req),
-        };
-        FutureResponse { state }
+        FutureResponse::new(self.upstream.call(req))
     }
 }
 
