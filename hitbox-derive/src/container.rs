0e44edd6fe94a39fab6343b5aeded4dfdd07165a
Denis Kayshev <topenkoff@gmail.com>
2023-03-29 00:18:04 +0300

    update syn

--- hitbox-derive/src/container.rs
+++ hitbox-derive/src/container.rs
@@ -3,17 +3,20 @@ use quote::ToTokens;
 const CACHE_TTL: &str = "cache_ttl";
 const CACHE_STALE_TTL: &str = "cache_stale_ttl";
 const CACHE_VERSION: &str = "cache_version";
+const HITBOX: &str = "hitbox";
 
-fn parse_lit_to_u32(lit: &syn::Lit, attr_name: &str) -> syn::Result<u32> {
-    match lit {
-        syn::Lit::Int(lit) => lit
-            .base10_parse::<u32>()
-            .map_err(|e| syn::Error::new_spanned(lit, e)),
-        _ => Err(syn::Error::new_spanned(
-            lit,
-            format!("Expected hitbox {attr_name} attribute should be u32"),
-        )),
+fn parse_lit_to_u32(expr: &syn::Expr, attr_name: &str) -> syn::Result<u32> {
+    if let syn::Expr::Lit(expr) = expr {
+        if let syn::Lit::Int(lit) = &expr.lit {
+            return lit
+                .base10_parse::<u32>()
+                .map_err(|e| syn::Error::new_spanned(lit, e));
+        }
     }
+    Err(syn::Error::new_spanned(
+        expr,
+        format!("Expected hitbox {attr_name} attribute should be u32"),
+    ))
 }
 
 pub struct Container {
@@ -32,12 +35,16 @@ impl Container {
             .attrs
             .iter()
             .map(|attr| {
-                if !attr.path.is_ident("hitbox") {
+                if !attr.path().is_ident(HITBOX) {
                     return Ok(Vec::new());
                 }
-                match attr.parse_meta() {
-                    Ok(syn::Meta::List(meta)) => Ok(meta.nested.into_iter().collect()),
-                    Ok(other) => Err(syn::Error::new_spanned(other, "expected #[hitbox(...)]")),
+
+                let nested = attr.parse_args_with(
+                    syn::punctuated::Punctuated::<syn::Meta, syn::Token![,]>::parse_terminated,
+                );
+
+                match nested {
+                    Ok(nested) => Ok(nested.into_iter().collect()),
                     Err(err) => Err(err),
                 }
             })
@@ -48,38 +55,30 @@ impl Container {
         for meta_item in items {
             match &meta_item {
                 // Parse `#[hitbox(cache_ttl = 42)]`
-                syn::NestedMeta::Meta(syn::Meta::NameValue(m)) if m.path.is_ident(CACHE_TTL) => {
-                    ttl = Some(parse_lit_to_u32(&m.lit, CACHE_TTL)?);
+                syn::Meta::NameValue(m) if m.path.is_ident(CACHE_TTL) => {
+                    ttl = Some(parse_lit_to_u32(&m.value, CACHE_TTL)?);
                 }
 
                 // Parse `#[hitbox(cache_stale_ttl = 42)]`
-                syn::NestedMeta::Meta(syn::Meta::NameValue(m))
-                    if m.path.is_ident(CACHE_STALE_TTL) =>
-                {
-                    stale_ttl = Some(parse_lit_to_u32(&m.lit, CACHE_STALE_TTL)?);
+                syn::Meta::NameValue(m) if m.path.is_ident(CACHE_STALE_TTL) => {
+                    stale_ttl = Some(parse_lit_to_u32(&m.value, CACHE_STALE_TTL)?);
                 }
 
                 // Parse `#[hitbox(cache_version = 42)]`
-                syn::NestedMeta::Meta(syn::Meta::NameValue(m))
-                    if m.path.is_ident(CACHE_VERSION) =>
-                {
-                    version = Some(parse_lit_to_u32(&m.lit, CACHE_VERSION)?);
+                syn::Meta::NameValue(m) if m.path.is_ident(CACHE_VERSION) => {
+                    version = Some(parse_lit_to_u32(&m.value, CACHE_VERSION)?);
                 }
 
                 // Throw error on unknown attribute
-                syn::NestedMeta::Meta(m) => {
-                    let path = m.path().into_token_stream().to_string().replace(' ', "");
+                meta_value => {
+                    let path = meta_value
+                        .path()
+                        .into_token_stream()
+                        .to_string()
+                        .replace(' ', "");
                     return Err(syn::Error::new_spanned(
-                        m.path(),
+                        meta_value.path(),
                         format!("Unknown hitbox container attribute `{path}`"),
-                    ));
-                }
-
-                // Throw error on other lit types
-                lit => {
-                    return Err(syn::Error::new_spanned(
-                        lit,
-                        "Unexpected literal in hitbox container attribute",
                     ));
                 }
             }
