a899e8fa2fa665490329d5888f9982f0f717b63a
lowit <ea@lowit.ru>
2023-08-14 02:18:43 +0300

    tarantool: improve tests

--- hitbox-tarantool/tests/integration_test.rs
+++ hitbox-tarantool/tests/integration_test.rs
@@ -58,7 +58,7 @@ impl<'a> StartedTarantool<'a> {
         R: Deserialize<'a>,
     {
         self.client
-            .prepare_fn_call(cmd)
+            .prepare_fn_call(format!("box.space.hitbox_cache:{}", cmd))
             .bind_ref(params)
             .unwrap()
             .execute()
@@ -110,7 +110,7 @@ async fn test_init() {
     let space_exists: (bool,) = t
         .eval(
             "return box.space[...] and true or false",
-            &("hitbox_cache".to_string(),),
+            &("hitbox_cache",),
         )
         .await;
     assert!(space_exists.0);
@@ -118,7 +118,7 @@ async fn test_init() {
     let fiber_exists: (bool,) = t
         .eval(
             "return _G[...] and true or false",
-            &("__hitbox_cache_fiber".to_string(),),
+            &("__hitbox_cache_fiber",),
         )
         .await;
     assert!(fiber_exists.0);
@@ -161,8 +161,8 @@ struct TarantoolTuple {
 #[tokio::test]
 async fn test_set() {
     let t = Tarantool::start().await;
-    let key = "test_key".to_owned();
-    let dt = "2012-12-12T12:12:12Z".to_string();
+    let key = "test_key".to_string();
+    let dt = "2012-12-12T12:12:12Z";
     let ttl = 42;
     let value = CachedValue::new(Test::new(), DateTime::from_str(&dt).unwrap());
 
@@ -171,7 +171,7 @@ async fn test_set() {
         .await
         .unwrap();
 
-    let result: Vec<TarantoolTuple> = t.call("box.space.hitbox_cache:get", &(key.clone())).await;
+    let result: Vec<TarantoolTuple> = t.call("get", &(key.clone())).await;
 
     assert_eq!(
         result.first().unwrap(),
@@ -187,61 +187,49 @@ async fn test_set() {
 async fn test_expire() {
     let t = Tarantool::start().await;
     let key = "test_key".to_owned();
-    let dt = "2012-12-12T12:12:12Z".to_string();
-    let ttl = 0;
+    let dt = "2012-12-12T12:12:12Z";
     let value = CachedValue::new(Test::new(), DateTime::from_str(&dt).unwrap());
 
     t.backend
-        .set::<Test>(key.clone(), &value, Some(ttl))
+        .set::<Test>(key.clone(), &value, Some(0))
         .await
         .unwrap();
 
     thread::sleep(Duration::from_secs(1));
 
-    let result: Vec<TarantoolTuple> = t.call("box.space.hitbox_cache:get", &(key.clone())).await;
-
+    let result: Vec<TarantoolTuple> = t.call("get", &(key)).await;
     assert_eq!(result.len(), 0)
 }
 
 #[tokio::test]
 async fn test_delete() {
     let t = Tarantool::start().await;
-    let key = "test_key".to_owned();
-    let value = r#"{"data":{"a":42,"b":"nope"},"expired":"2012-12-12T12:12:12Z"}"#.to_string();
-    let ttl = 42;
+    let key = "test_key";
+    let value = r#"{"data":{"a":42,"b":"nope"},"expired":"2012-12-12T12:12:12Z"}"#;
 
-    let status = t.backend.delete(key.clone()).await.unwrap();
+    let status = t.backend.delete(key.to_string()).await.unwrap();
     assert_eq!(status, DeleteStatus::Missing);
 
-    t.call::<_, (String, Option<u32>, String)>(
-        "box.space.hitbox_cache:replace",
-        &("test_key".to_string(), ttl, value),
-    )
-    .await;
-
-    let status = t.backend.delete(key.clone()).await.unwrap();
-    assert_eq!(status, DeleteStatus::Deleted(1));
-
-    let result: Vec<(String, Option<u32>, String)> = t
-        .call("box.space.hitbox_cache:get", &("test_key".to_string()))
+    t.call::<_, TarantoolTuple>("replace", &(key, 42, value))
         .await;
 
+    let status = t.backend.delete(key.to_string()).await.unwrap();
+    assert_eq!(status, DeleteStatus::Deleted(1));
+
+    let result: Vec<TarantoolTuple> = t.call("get", &(key)).await;
     assert_eq!(result.len(), 0)
 }
 
 #[tokio::test]
 async fn test_get() {
     let t = Tarantool::start().await;
-    let key = "test_key".to_owned();
-    let value = r#"{"data":{"a":42,"b":"nope"},"expired":"2012-12-12T12:12:12Z"}"#.to_string();
+    let key = "test_key";
+    let value = r#"{"data":{"a":42,"b":"nope"},"expired":"2012-12-12T12:12:12Z"}"#;
 
-    t.call::<_, (String, Option<u32>, String)>(
-        "box.space.hitbox_cache:replace",
-        &(key.clone(), 42, value),
-    )
-    .await;
+    t.call::<_, TarantoolTuple>("replace", &(key, 42, value))
+        .await;
 
-    let data = t.backend.get::<Test>(key).await.unwrap().unwrap();
+    let data = t.backend.get::<Test>(key.into()).await.unwrap().unwrap();
     let dt: DateTime<Utc> = DateTime::from_str(&"2012-12-12T12:12:12Z").unwrap();
 
     assert_eq!(data.data, Test::new());
