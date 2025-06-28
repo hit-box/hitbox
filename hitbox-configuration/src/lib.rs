f3863acc73a297a58166c673e9df7f9175241824
Andrey Ermilov <andrerm@ya.ru>
2025-06-28 11:48:47 +0200

    feat(configuration): fix expression

--- hitbox-configuration/src/lib.rs
+++ hitbox-configuration/src/lib.rs
@@ -1,43 +1,52 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+#[serde(untagged)]
 pub enum HeaderOperation {
     Eq { name: String, value: String },
     Exist(String),
     In(String, Vec<String>),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+#[serde(untagged)]
 pub enum QueryOperation {
     Eq { name: String, value: String },
     Exist(String),
     In(String, Vec<String>),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub enum Predicate {
     Method(String),
+    Path(String),
     Query(QueryOperation),
     Header(HeaderOperation),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub enum Expression {
-    Predicate(Predicate),
-    And(Box<Expression>, Box<Expression>),
-    Or(Box<Expression>, Box<Expression>),
+    And(Box<Wrapper>, Box<Wrapper>),
+    Or(Box<Wrapper>, Box<Wrapper>),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+#[serde(untagged)]
+pub enum Wrapper {
+    Predicate(Predicate),
+    Expression(Expression),
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 #[serde(untagged)]
 pub enum Request {
     #[serde(rename = "requests")]
     Flat(Vec<Predicate>),
     #[serde(rename = "request")]
-    Tree(Expression),
+    Tree(Wrapper),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub struct Endpoint {
     #[serde(with = "serde_yaml::with::singleton_map_recursive")]
     pub request: Request,
--- hitbox-configuration/tests/test_configuration.rs
+++ hitbox-configuration/tests/test_configuration.rs
@@ -1,31 +1,99 @@
-use hitbox_configuration::{
-    Endpoint, Expression, HeaderOperation, Predicate, QueryOperation, Request,
-};
+use hitbox_configuration::{Endpoint, Expression, Predicate, QueryOperation, Request, Wrapper};
 
 #[test]
-fn test_expression_tree() {
-    let header = Predicate::Header(HeaderOperation::Eq {
-        name: "X-Cache".to_owned(),
+fn test_expression_tree_serialize() {
+    let method = Wrapper::Predicate(Predicate::Method("GET".to_owned()));
+    let path = Wrapper::Predicate(Predicate::Path("/books".to_owned()));
+    let query = Wrapper::Predicate(Predicate::Query(QueryOperation::Eq {
+        name: "cache".to_owned(),
         value: "true".to_owned(),
-    });
+    }));
+    let and_ = Wrapper::Expression(Expression::And(method.into(), path.into()));
+    let or_ = Wrapper::Expression(Expression::Or(query.into(), and_.into()));
+    let request = Request::Tree(or_);
+    let endpoint = Endpoint { request };
+    let yaml_str = serde_yaml::to_string(&endpoint).unwrap();
+    let expected = r"request:
+  Or:
+  - Query:
+      name: cache
+      value: 'true'
+  - And:
+    - Method: GET
+    - Path: /books
+";
+    assert_eq!(yaml_str, expected);
+}
+
+#[test]
+fn test_expression_tree_deserialize() {
+    let yaml_str = r"request:
+  Or:
+  - Query:
+      name: cache
+      value: 'true'
+  - And:
+    - Method: GET
+    - Path: /books
+";
+    let endpoint: Endpoint = serde_yaml::from_str(&yaml_str).unwrap();
+
+    let method = Wrapper::Predicate(Predicate::Method("GET".to_owned()));
+    let path = Wrapper::Predicate(Predicate::Path("/books".to_owned()));
+    let query = Wrapper::Predicate(Predicate::Query(QueryOperation::Eq {
+        name: "cache".to_owned(),
+        value: "true".to_owned(),
+    }));
+    let and_ = Wrapper::Expression(Expression::And(method.into(), path.into()));
+    let or_ = Wrapper::Expression(Expression::Or(query.into(), and_.into()));
+    let request = Request::Tree(or_);
+    let expected = Endpoint { request };
+
+    assert_eq!(endpoint, expected);
+}
+
+#[test]
+fn test_expression_flat_serialize() {
+    let method = Predicate::Method("GET".to_owned());
+    let path = Predicate::Path("/books".to_owned());
     let query = Predicate::Query(QueryOperation::Eq {
         name: "cache".to_owned(),
         value: "true".to_owned(),
     });
+    let request = Request::Flat(vec![method, path, query]);
+    let endpoint = Endpoint { request };
+    let yaml_str = serde_yaml::to_string(&endpoint).unwrap();
+    println!("{}", yaml_str);
+    let expected = r"request:
+- Method: GET
+- Path: /books
+- Query:
+    name: cache
+    value: 'true'
+";
+    assert_eq!(yaml_str, expected);
+}
+
+#[test]
+fn test_expression_flat_deserialize() {
+    let yaml_str = r"request:
+- Method: GET
+- Path: /books
+- Query:
+    name: cache
+    value: 'true'
+";
+    let endpoint: Endpoint = serde_yaml::from_str(&yaml_str).unwrap();
+
     let method = Predicate::Method("GET".to_owned());
-
-    let expression = Expression::And(
-        Box::new(Expression::Or(
-            Box::new(Expression::Predicate(header)),
-            Box::new(Expression::Predicate(query)),
-        )),
-        Box::new(Expression::Predicate(method)),
-    );
-
-    let endpoint = Endpoint { request: Request::Tree(expression) };
-    let rep = serde_yaml::to_string(&endpoint).unwrap();
-    println!("{}", &rep);
-    let endpoint : Endpoint = serde_yaml::from_str(rep.as_str()).unwrap();
-    dbg!(endpoint);
-    unreachable!()
+    let path = Predicate::Path("/books".to_owned());
+    let query = Predicate::Query(QueryOperation::Eq {
+        name: "cache".to_owned(),
+        value: "true".to_owned(),
+    });
+    let request = Request::Flat(vec![method, path, query]);
+    let expected = Endpoint { request };
+    let yaml_str = serde_yaml::to_string(&endpoint).unwrap();
+    println!("{}", yaml_str);
+    assert_eq!(endpoint, expected);
 }
